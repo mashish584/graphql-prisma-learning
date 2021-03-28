26c605c915e061b65e527feeda64f86b9f206a15
Ashish Mehra <singhashu163@gmail.com>
2021-03-28 15:19:35 +0530

    Fetch authenticated user posts test case added

--- tests/post.test.js
+++ tests/post.test.js
@@ -1,7 +1,7 @@
 import "cross-fetch/polyfill";
 import { gql } from "apollo-boost";
 
-import seedDatabase from "./utils/seedDatabase";
+import seedDatabase, { user } from "./utils/seedDatabase";
 import getClient from "./utils/getClient";
 
 const client = getClient();
@@ -31,4 +31,21 @@ test("Should return published posts", async () => {
   expect(posts.length).toBe(1);
   expect(posts[0].published).toBe(true);
   expect(posts[0].author.email).toBe(null);
+});
+
+test("Should return all my posts", async () => {
+  const client = getClient(user.token);
+  const postQuery = gql`
+    query {
+      myPosts {
+        id
+        title
+      }
+    }
+  `;
+
+  const { data } = await client.query({ query: postQuery });
+
+  expect(data.myPosts[0].title).toBe("Sample 2");
+  expect(data.myPosts[1].title).toBe("Sample 3");
 });
